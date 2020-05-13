5e5e4be50ffcaa5997dff62d4f0142e66815424e
Manwej <jeronimo.landauer@gmail.com>
2020-05-13 10:57:26 +0200

    first commit

---
+++ routes/users.js
@@ -0,0 +1,72 @@
+const express = require('express')
+const router=express.Router()
+const User= require('../models/user')
+
+//GET all users from /users/
+
+router.get('/', async(req,res)=>{
+    try{
+        const users = await User.find()
+        res.send(users)
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+//getting one
+router.get('/:id',getUser, (req,res)=>{
+    res.json(res.user)
+})
+//Creating one
+router.post('/', async(req,res)=>{
+    const user = new User({
+        name: req.body.name,
+      email: req.body.email,
+      password: req.body.password
+    })
+    try{
+        const newUser = await user.save()
+        res.status(201).json(newUser)
+    }catch(err){
+        res.status(400).json({message: err.message})
+    }
+})
+//Update one
+router.patch('/:id',getUser, async (req, res)=>{
+    if(req.body.name!=null){
+        res.user.name=req.body.name;
+      }
+      if(req.body.email!=null){
+        res.user.email=req.body.email;
+      }
+      if(req.body.password!=null){
+        res.user.password=req.body.password;
+      }
+      try{
+        const updatedUser = await res.user.save()
+        res.json(updatedUser)
+        
+      }catch(err){
+        res.status(400).json({message: err.message})
+      }
+})
+// deleting one
+router.delete('/:id',getUser, async(req,res)=>{
+    try{
+        await res.user.remove()
+        res.json({message: "user deleted"})
+    }catch(err){
+        res.status(500).json({message: err.message})
+    }
+})
+async function getUser (req,res,next){
+    let user
+    try{
+        user = await User.findById(req.params.id)
+        if (user==null){return res.status(404).json({message: "cannot find user"})}
+    }catch(err){
+        return res.status(500).json({message: err.message})
+    }
+    res.user=user;
+    next()
+}
+module.exports=router
---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const mongoose= require('mongoose')
+mongoose.set('useNewUrlParser', true)
+mongoose.connect(process.env.DATABASE_URL, {useUnifiedTopology: true})
+
+const db= mongoose.connection;
+
+db.on('error', (error)=>console.log(error))
+db.once('open', ()=>console.log("connected to database"))
+app.use(express.json())//a. express.json() is a method inbuilt in express to recognize the incoming Request Object as a JSON Object. This method is called as a middleware in your application using the code: app.use(express.json());
+const userRouter=require('./routes/users')
+
+app.use('/users', userRouter)
+
+app.listen(5000, ()=>{
+    console.log("server has started")
+})
